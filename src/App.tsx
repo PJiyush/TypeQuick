6a3dd90eb0a5ac8cfe0b4ffe801c0a9db34604f6
PJiyush <jhamnani.1@iitj.ac.in>
2024-08-11 11:59:58 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {Button} from './components/ui/button'
+function App() {
+  return (
+    <>
+      <Button variant="link">Click me</Button>
+    </>
+  )
+}
+
+export default App
