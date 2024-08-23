89204fac2b1966ae787a135fc845eba8e9822dc6
PJiyush <jhamnani.1@iitj.ac.in>
2024-08-24 01:21:42 +0530

    implement start key

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -6,22 +6,46 @@ import { useTimer } from '@/hooks/useTimer';
 import { useStart } from '@/context/StartContext';
 
 const words:string = generateWords(10)
+
 function Terminal() {
     const {isStart,switchMode} = useStart()||{};
+    
     const timer = useTimer(isStart);
     useEffect(() => {
-        document.addEventListener('keydown',handleKey);
+        const startKey = (e:KeyboardEvent)=>{
+            e.preventDefault();
+            if(e.key === 'Enter'){
+                if(switchMode) switchMode()
+                else console.log('switchMode is not defined')
+        }}
+        if(!isStart){
+            document.addEventListener('keydown',startKey)
+        } 
+        return () => {
+            document.removeEventListener('keydown',startKey)
+        }
+    },[isStart])
+    useEffect(() => {
+        if(isStart){
+            document.addEventListener('keydown',handleKey);
+            document.addEventListener('visibilitychange',()=>{
+                if(document.hidden && switchMode) switchMode()
+            })
+        }
         return () => {
             document.removeEventListener('keydown',handleKey);
+            document.removeEventListener('visibilitychange',()=>{
+                if(document.hidden && switchMode) switchMode()
+            })
         }
-    },[])
+    },[isStart, switchMode])
     useEffect(() => {
         appendClass( document.querySelector('.word'), 'current');
         appendClass( document.querySelector('.character'), 'focus');
     },[])
     return (
         <div className='terminal h-3/4 w-5/6 mx-auto my-10 rounded-3xl bg-white/20 shadow-lg shadow-black/10'>
-            <div className={`h-96 mx-8 pt-8`} id='words'>
+            <div className={`h-96 mx-8 pt-8 ${isStart?"":'blur-lg'}`} id='words'>
                 {words.split(' ').map((word:string, index:number) => (
                     <span key={index} className="text-5xl font-mono word">
                         {word.split("").map((char, index) => (
@@ -31,10 +55,13 @@ function Terminal() {
                     </span>
                 ))}
             </div>
+            <div className={` mx-8 ${isStart?'hidden':''} -top-96 relative pt-8`} >
+                <p className=' text-5xl text-white'>Press Enter to start</p>
+            </div>
             <div className='bg-yellow-500 text-white w-10 text-center rounded-md'>
                 {timer}
             </div>
-            <div onClick={switchMode}>
+            <div onClick={switchMode} className='bg-red-600 w-52'>
                 stop
             </div>
         </div>
